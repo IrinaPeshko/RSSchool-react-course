477cfbc36dbe712fa9ba6f70aa292cd413295f77
IrinaPeshko <privet-kira1999@mail.ru>
2023-10-30 17:16:36 +0800

    fix: change app to class component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,15 +1,18 @@
 import styles from './App.module.css';
 import SearchInput from '../search-input/SearchInput';
 import ErrorBoundary from '../error-boundary/ErrorBoundary';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <ErrorBoundary>
-      <div className={styles.content}>
-        <SearchInput />
-      </div>
-    </ErrorBoundary>
-  );
+class App extends Component {
+  render() {
+    return (
+      <ErrorBoundary>
+        <div className={styles.content}>
+          <SearchInput />
+        </div>
+      </ErrorBoundary>
+    );
+  }
 }
 
 export default App;

1f3e9a98cd73a9c52d276f0cdab7693fedb781c6
IrinaPeshko <privet-kira1999@mail.ru>
2023-11-05 21:10:02 +0800

    fix: base in router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,17 +2,22 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from '../components/app/App';
 import CardDetail from '../components/card-detail/CardDetail';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          path: 'details/:cardId',
+          element: <CardDetail />,
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: 'details/:cardId',
-        element: <CardDetail />,
-      },
-    ],
-  },
-]);
+    basename: '/RSSchool-react-course',
+  }
+);
 
 export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ export default defineConfig({
   build: {
     outDir: 'build',
   },
-  base: '',
+  base: '/RSSchool-react-course',
 });

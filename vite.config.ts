98d517a678860002a369f92abcc2ed9919f816c5
IrinaPeshko <privet-kira1999@mail.ru>
2023-11-09 13:25:45 +0800

    feat: set vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -7,4 +8,11 @@ export default defineConfig({
     outDir: 'build',
   },
   base: '/RSSchool-react-course',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    // coverage:{
+    //   reporter:['text', 'json', 'html'] // change this property to the desired output
+    // }
+  },
 });

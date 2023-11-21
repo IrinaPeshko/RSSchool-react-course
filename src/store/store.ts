5cfe66998e229508896de3b932dec52339a88131
IrinaPeshko <privet-kira1999@mail.ru>
2023-11-22 00:15:22 +0800

    feat: add store

---
+++ src/store/store.ts
@@ -0,0 +1,21 @@
+// import { reduxApi } from '../api/reduxApi';
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import queryParamsReducer from './reducers/queryParams';
+import isLoading from './reducers/isLoading';
+
+export const rootReducer = combineReducers({
+  // [reduxApi.reducerPath]: reduxApi.reducer,
+  queryParamsReducer,
+  isLoading,
+});
+
+export const store = configureStore({
+  reducer: rootReducer
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware().concat(reduxApi.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppStore = typeof store;
+export type AppDispatch = AppStore['dispatch'];
